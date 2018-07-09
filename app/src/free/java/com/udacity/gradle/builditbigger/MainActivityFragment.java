6c90fb6b744f9457e7379b300a16b83c6e897f4b
juak <l0ndr1na0901>
2018-07-09 17:27:02 -0300

    Interstitial Ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -14,8 +14,10 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 import guskuma.com.jokeactivities.JokeActivity;
 
@@ -28,6 +30,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
     private Loader<String> mLoader;
     private ProgressBar mProgressBar;
     private Button mTellJokeButton;
+    private InterstitialAd mInterstitialAd;
 
     public MainActivityFragment() {
     }
@@ -37,14 +40,12 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+        AdView mAdView = root.findViewById(R.id.adView);
+        mAdView.loadAd(new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build());
+
+        mInterstitialAd = new InterstitialAd(this.getActivity());
+        mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
+        mInterstitialAd.loadAd(new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build());
 
         mProgressBar = root.findViewById(R.id.progress_bar);
         mTellJokeButton = root.findViewById(R.id.tellJokeButton);
@@ -71,7 +72,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
     }
 
     @Override
-    public void onLoadFinished(@NonNull Loader<String> loader, String s) {
+    public void onLoadFinished(@NonNull Loader<String> loader, final String s) {
 
         mProgressBar.findViewById(R.id.progress_bar).setVisibility(View.INVISIBLE);
         mTellJokeButton.setEnabled(true);
@@ -81,6 +82,21 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
             return;
         }
 
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.setAdListener(new AdListener(){
+                                              @Override
+                                              public void onAdClosed() {
+                                                  startJokeActivity(s);
+                                              }
+                                          }
+            );
+            mInterstitialAd.show();
+        } else {
+            startJokeActivity(s);
+        }
+    }
+
+    private void startJokeActivity(String s) {
         Intent intent = new Intent(getActivity(), JokeActivity.class);
         intent.putExtra(JokeActivity.PARAM_JOKE, s);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
